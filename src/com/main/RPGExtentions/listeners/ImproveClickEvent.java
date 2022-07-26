dc99ccc7a7ded7da29043ab8eb9fac25b74afe42
Fire_Whotao <821896444@qq.com>
2022-07-26 10:38:30 +0800

    fix a bug

--- src/com/main/RPGExtentions/listeners/ImproveClickEvent.java
+++ src/com/main/RPGExtentions/listeners/ImproveClickEvent.java
@@ -247,7 +247,6 @@ public class ImproveClickEvent implements Listener {
                         if (Objects.requireNonNull(inv.getItem(id)).equals(instance.up)) {
                             if (cur != null) {
                                 int amount = cur.getAmount();
-                                cur.setAmount(1);
                                 if (!((cur.equals(instance.highup)) || (cur.equals(instance.midup)) || (cur.equals(instance.lowup)))) {
                                     p.sendMessage("§e§l请放入正确的强化石");
                                     e.setCancelled(true);
@@ -256,6 +255,7 @@ public class ImproveClickEvent implements Listener {
                                         p.sendMessage("§e§l请先放入武器");
                                         e.setCancelled(true);
                                     } else {
+                                        cur.setAmount(1);
                                         e.setCancelled(true);
                                         inv.setItem(15, cur);
                                         if (amount > 1) {
@@ -286,12 +286,12 @@ public class ImproveClickEvent implements Listener {
                         if (Objects.requireNonNull(inv.getItem(id)).equals(instance.help)) {
                             if (cur != null) {
                                 int amount = cur.getAmount();
-                                cur.setAmount(1);
                                 if (!(cur.equals(instance.highkeep) || cur.equals(instance.lowkeep))) {
                                     p.sendMessage("§e§l请放入正确的保护符");
                                     e.setCancelled(true);
                                 } else {
                                     e.setCancelled(true);
+                                    cur.setAmount(1);
                                     inv.setItem(id, cur);
                                     if (amount > 1) {
                                         cur.setAmount(amount - 1);
